9fdf9377d389a6f9c04e0a7444eb75ede1c4f4dc
Jan Ooms <jan@creatiom.be>
2024-09-04 10:09:50 +0200

    Fixes for Symfony 7.1

--- DependencyInjection/WaazOpengraphExtension.php
+++ DependencyInjection/WaazOpengraphExtension.php
@@ -1,25 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Waaz\OpengraphBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
- * This is the class that loads and manages your bundle configuration
+ * This is the class that loads and manages your bundle configuration.
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
 class WaazOpengraphExtension extends Extension implements PrependExtensionInterface
 {
-
-    /**
-     * {@inheritdoc}
-     */
-    public function prepend(ContainerBuilder $container)
+    public function prepend(ContainerBuilder $container): void
     {
         $configs = $container->getExtensionConfig($this->getAlias());
         $resolvingBag = $container->getParameterBag();
@@ -31,7 +29,6 @@ class WaazOpengraphExtension extends Extension implements PrependExtensionInterf
                 'exception_controller' => null,
             ]);
         }
-        
 
         if ($container->hasExtension('sulu_admin')) {
             $container->prependExtensionConfig(
@@ -42,7 +39,7 @@ class WaazOpengraphExtension extends Extension implements PrependExtensionInterf
                             __DIR__ . '/../Resources/config/forms',
                         ],
                     ],
-                ]
+                ],
             );
         }
 
@@ -54,20 +51,17 @@ class WaazOpengraphExtension extends Extension implements PrependExtensionInterf
                         __DIR__ . '/../Resources/config/image-formats.xml',
                         '%kernel.project_dir%/config/image-formats.xml',
                     ],
-                ]
+                ],
             );
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
     }
 }
